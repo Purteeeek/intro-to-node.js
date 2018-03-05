4bd3f22c3a51bbc2ae1de15e59c842bc9f710623
Purteeeek <deshpandepratikr@gmail.com>
2018-03-06 00:48:27 +0530

    Added lodash as a dependancy. Package.json and require third party app.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,8 +1,14 @@
 console.log('Starting app.js.')
 const fs = require('fs')
 const os = require('os')
+const _ = require('lodash')
 const notes = require('./notes.js');
 
-var user = os.userInfo()
-console.log(notes.add(3,4))
-fs.appendFileSync('greetings.txt',`Hello ${user.username} age is ${notes.age}`)
+// var user = os.userInfo()
+// console.log(notes.add(3,4))
+// fs.appendFileSync('greetings.txt',`Hello ${user.username} age is ${notes.age}`)
+
+console.log(_.isString(true));
+console.log(_.isString('Panda'));
+var filteredArray = _.uniq([1,2,2,1,34,5]);
+console.log(filteredArray);
